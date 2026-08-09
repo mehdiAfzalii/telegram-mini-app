0a4f03d5cbb60e9ad1e0b1199ba168f0552ba206
Mehdi Afzali <afzalimahdi31@gmail.com>
2026-08-10 01:37:07 +0330

    update

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+// const tg = window.Telegram.WebApp;
+
+// tg.ready();
+
+// console.log(tg.colorScheme);
+// console.log(tg.themeParams);
+
+// const button = document.getElementById("btn");
+
+// button.addEventListener("click", function () {
+//     alert("Hello From Swan");
+// });
+
+// تست اطلاعات کاربر
+
 const tg = window.Telegram.WebApp;
 
 tg.ready();
 
-console.log(tg.colorScheme);
-console.log(tg.themeParams);
+const user = tg.initDataUnsafe.user;
 
-const button = document.getElementById("btn");
-
-button.addEventListener("click", function () {
-    alert("Hello From Swan");
-});
+console.log(user);
